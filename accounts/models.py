b05e88ea989200407965ca9885ce332ab9d457ff
2nithin2 <official.nithinraj@gmail.com>
2025-03-07 19:33:28 +0530

    fixed model.py and added authuser in settings.py working as expected

--- accounts/models.py
+++ accounts/models.py
@@ -2,14 +2,13 @@ from django.contrib.auth.models import AbstractUser, Group, Permission
 from django.db import models
 
 class CustomUser(AbstractUser):
+    email = models.EmailField(unique=True)
+
     groups = models.ManyToManyField(Group, related_name="customuser_groups")
     user_permissions = models.ManyToManyField(Permission, related_name="customuser_permissions")
 
-    # email = models.EmailField(unique=True)
-
-    # USERNAME_FIELD = 'email'  # Use email for authentication
-    # REQUIRED_FIELDS = ['username']
-
+    USERNAME_FIELD = 'email'  # Use email for authentication
+    REQUIRED_FIELDS = ['username']  # Keep username as required for superusers
 
     def __str__(self):
         return self.email
